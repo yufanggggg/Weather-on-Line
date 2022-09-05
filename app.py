22d2613ccca31a1b01821ac0659ed927b7627ec8
Yuuuuu Tsengggg <jeremy942712027@gmail.com>
2022-09-05 22:15:26 +0800

    Forth Commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, request, abort
 
 from linebot import (
@@ -37,10 +38,30 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    message=text=event.message.text
+    url="https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=rdec-key-123-45678-011121314"
+    data=requests.get(url)
+    data_json = data.json()    # 轉換成 JSON 格式
+
+    placesqty=len(data_json['records']['location'])
+    weatherqty=len(data_json['records']['location'][0]['weatherElement'])
+    weathertime=len(data_json['records']['location'][0]['weatherElement'][0]['time'])
+
+    def datainfo(search):
+        for i in range(placesqty):
+            if message==data_json['records']['location'][i]['locationName']:
+                print(data_json['records']['location'][i]['locationName'])       
+                for j in range(weathertime):
+                    print(data_json['records']['location'][i]['weatherElement'][0]['time'][j]['startTime']+"-"+data_json['records']['location'][i]['weatherElement'][0]['time'][j]['endTime'])
+                    print(data_json['records']['location'][i]['weatherElement'][0]['time'][j]['parameter']['parameterName'])
+                    print("降雨機率:"+data_json['records']['location'][i]['weatherElement'][1]['time'][j]['parameter']['parameterName']+"%")
+                    print(data_json['records']['location'][i]['weatherElement'][2]['time'][j]['parameter']['parameterName']+"度-"+data_json['records']['location'][i]['weatherElement'][4]['time'][j]['parameter']['parameterName']+"度")
+                    print(data_json['records']['location'][i]['weatherElement'][3]['time'][j]['parameter']['parameterName'])
+    # line_bot_api.reply_message(
+    #     event.reply_token,
+    #     TextSendMessage(text=event.message.text))
+
 
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run()
